dc6afb534bb82ce2636e4446f87309f03b457f9e
nohy6630 <nohy6630@gmail.com>
2024-09-17 19:42:09 +0900

    fix: json exception 핸들링

--- src/main/kotlin/com/yeongjin/kopring/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/yeongjin/kopring/global/exception/GlobalExceptionHandler.kt
@@ -1,10 +1,15 @@
 package com.yeongjin.kopring.global.exception
 
 import com.yeongjin.kopring.global.dto.ApiResult
-import com.yeongjin.kopring.global.exception.ExceptionContent.UNKNOWN_ERROR
+import com.yeongjin.kopring.global.exception.ExceptionContent.*
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.ErrorResponse
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.NoHandlerFoundException
+
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -13,6 +18,14 @@ class GlobalExceptionHandler {
         exception: CustomException
     ): ResponseEntity<ApiResult<Unit>> {
         return ApiResult.error<Unit>(exception.content)
+            .toResponseEntity()
+    }
+
+    @ExceptionHandler
+    fun handleJsonException(
+        exception: HttpMessageNotReadableException
+    ): ResponseEntity<ApiResult<Unit>> {
+        return ApiResult.error<Unit>(INVALID_JSON_FORMAT)
             .toResponseEntity()
     }
 
